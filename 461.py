27bf9ec738db005004431284234c6aeb97a8b36d
zhangning <1305113016@qq.com>
2020-03-25 10:33:37 +0800

    补齐字符串，使用双指针

--- 461.py
+++ 461.py
@@ -1,3 +1,20 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         return bin( x^y ).count('1')
+
+class Solution2:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xb = bin(x)[2:]
+        yb = bin(y)[2:]
+        l = max( len(xb), len(yb) )
+
+        xb = xb.zfill(l)
+        yb = yb.zfill(l)
+        res = 0
+        for i in range(l):
+            if xb[i] != yb[i]:
+                res += 1
+
+        return res
+
+
