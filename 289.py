56947e64f06dbc385b9fb67febb5c2464ec01689
zhangning <1305113016@qq.com>
2020-04-02 14:57:18 +0800

    增加题解：利用与运算高位存新值

--- 289.py
+++ 289.py
@@ -29,5 +29,43 @@ class Solution:
                 # for i in range(len(board)):
                 print("i: {}, j: {}".format(i, j))
 
+class Solution2:
+    """
+    使用 int 型数据的高位存放更新后的数据，低位存放原始数据，最后通过移位运算更新；
+    不必开辟额外的空间
+    """
+    def gameOfLife(self, board: list) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m, n = len(board), len(board[0])
+        dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+        dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+        for i in range(m):
+            for j in range(n):
+                tmpSum = 0
+                for k in range(8):
+                    x = i + dx[k]
+                    y = j + dy[k]
+                    if x<0 or x>=m or y<0 or y>=n:
+                        continue
+
+                    tmpSum += board[x][y] & 1
+
+                if (board[i][j] & 1)>0:
+                    if tmpSum==2 or tmpSum==3:
+                        board[i][j] = 0b11
+                    else:
+                        board[i][j] = 0b01
+                elif tmpSum==3:
+                    board[i][j] = 0b10
+
+        for i in range(m):
+            for j in range(n):
+                board[i][j] >>= 1
+
+
+
 Solution().gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]])
 
