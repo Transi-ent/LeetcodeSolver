8bbe636889d1a0de69b7fae9873dabad8e9ba8a9
zhangning <1305113016@qq.com>
2020-04-12 16:09:15 +0800

    三数之和

--- 15.py
+++ 15.py
@@ -15,25 +15,80 @@ def threeSum(nums: list, target: int):
             if d.get(complement):
                 return [complement, nums[i], nums[j]]
 
-
             d[complement] = 1
 
-def threeSum( nums: list) -> list[list[int]]:
+class Solution:
     """
-    As there are 3 numbers, even we use findTable, 2 for-loop
-    ..are still needed.
-    :param nums:
-    :return:
+    使用查找表，并将找到的结果排好序连成一个字符串放入哈希集合中
+    形成一个唯一性标识去重。
+    超时！！！
     """
-    d, res = {}, []
-    for i in range(len(nums)-2):
-        for j in range(i+1, len(nums)):
-            tmp = -(nums[i] + nums[j])
-            if d.get(tmp) and d[tmp]!=j:
-                res.append([nums[i], nums[j], tmp])
-
+    def threeSum(self, nums: list) -> list:
+        n = len(nums)
+        if n<3:
+            return []
+        map = {}
+        res,s = [], set()
+        for i, e in enumerate(nums):
+            if map.get(e):
+                map[e].append(i)
             else:
-                d[nums[j]] = j
+                map[e]=[i]
 
-    return res
+        for i in range(0, n-2):
+            for j in range(i+1, n-1):
+                tmp = nums[i] + nums[j]
+                print("i={}, j={}, n={}".format(i,j,n))
+                if i==0 and j==2:
+                    print("tmp: {}".format(tmp))
 
+                if map.get(-tmp):
+                    lyst = map.get(-tmp)
+                    for k in lyst:
+                        if k>j:
+                            tmp = [nums[i],nums[j],nums[k]]
+                            ts = ''.join([str(t) for t in sorted(tmp)])
+                            if not ts in s:
+                                res.append(tmp)
+                                s.add(ts)
+
+        print(res)
+        return res
+
+class Solution2:
+    def threeSum(self, nums: list) -> list:
+        n = len(nums)
+        if n<3:
+            return []
+        if all([i==0 for i in nums]):
+            return [[0,0,0]]
+        map = {}
+        res,s = [], set()
+        nums.sort()
+        for i, e in enumerate(nums):
+            if map.get(e):
+                map[e].append(i)
+            else:
+                map[e]=[i]
+
+        for i in range(0, n-2):
+            for j in range(i+1, n-1):
+                tmp = nums[i] + nums[j]
+                print("i={}, j={}, n={}".format(i,j,n))
+                if i==0 and j==2:
+                    print("tmp: {}".format(tmp))
+
+                if map.get(-tmp):
+                    lyst = map.get(-tmp)
+                    for k in lyst:
+                        if k>j:
+                            tmp = [nums[i],nums[j],nums[k]]
+                            ts = ''.join([str(t) for t in tmp])
+                            if not ts in s:
+                                res.append(tmp)
+                                s.add(ts)
+
+        print(res)
+        return res
+
+Solution2().threeSum([-1,0,1,2,-1,-4])
