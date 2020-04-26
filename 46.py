70670c2f1ff040775f557d343421224f45e1d2ff
zhangning <1305113016@qq.com>
2020-04-26 10:20:22 +0800

    全排列问题。回溯

--- 46.py
+++ 46.py
@@ -55,7 +55,45 @@ class Solution:
 
         return res
 
+class Solution2:
+    def permute(self, nums: list) -> list:
+
+        return self.findPermutation(set(nums), [], [])
+
+    def findPermutation(self, s: set, tmp: list, res: list)->list:
+        """
+        :param nums:
+        :param s: 可用元素集合；
+        :param tmp: 还未完整的一个数列；
+        :param res: 存放结果
+        :return:
+        """
+        if len(s)==0:
+            res.append(tmp)
+            return res
+
+        nums = list(s)
+        for num in nums:
+            copyOfS = s.copy()
+            copyOfS.remove(num)
+            copyOfList = tmp.copy()
+            copyOfList.append(num)
+            self.findPermutation( copyOfS, copyOfList, res)
+        return res
+
+class Solution3:
+    def permute(self, nums: list) -> list:
+        res = []
+        def traceback(nums: list, tmp: list):
+            if not nums: # 若 nums 数组为空，说明当前组合搜索已经到底
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                traceback(nums[:i]+nums[i+1:], tmp+[nums[i]])
+
+        traceback(nums, [])
+        return res
 
 
-res = Solution().permute2([1,2,3])
+res = Solution3().permute([1,2,3])
 print(res)
