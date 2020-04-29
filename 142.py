db64bcb526ec3ff7e25a1ee05ba58ceae25646db
zhangning <1305113016@qq.com>
2020-04-29 14:36:26 +0800

    寻找环的入口节点

--- 142.py
+++ 142.py
@@ -5,6 +5,10 @@ class ListNode:
         self.next = None
 
 class Solution:
+    """
+    双指针，使用一个额外的集合来记录所走过的节点。
+    时间复杂度 O(N),空间复杂度 O(b), b 为环长度
+    """
     def detectCycle(self, head: ListNode) -> ListNode:
         if head is None:
             return None
@@ -32,3 +36,24 @@ class Solution:
             cur = cur.next
             i += 1
         return cur
+
+class Solution2:
+    """
+    双指针，结合数学法
+    时间复杂度 O(N),空间复杂度 O(1)
+    假设 fast、slow 相遇时，slow 走了s，则fast走了 2s，fast 比 slow 多走了 n 圈
+    若从头结点到环入口点有 a 个节点，环上有 b 个节点，则相遇时 fast 走了 s+nb，
+    所以，s=nb，然后 slow 沿路继续走 a 步后路程变为 a + nb，即刚好回到环入口点。
+    """
+    def detectCycle(self, head: ListNode) -> ListNode:
+        fast = slow = head
+        while True:
+            if fast is None or fast.next is None:
+                return None
+            fast, slow = fast.next.next, slow.next
+            if fast == slow:
+                break
+        fast = head
+        while fast != slow:
+            fast, slow = fast.next, slow.next
+        return fast
