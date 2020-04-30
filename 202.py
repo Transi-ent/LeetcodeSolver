0d2aed26812a194ef442ac5732f68ee905febe4b
zhangning <1305113016@qq.com>
2020-04-30 10:39:58 +0800

    快乐数

--- 202.py
+++ 202.py
@@ -28,3 +28,39 @@ def isHappy2( n: int) -> bool:
             s.add(n)
 
     return n==1
+
+class Solution:
+    """
+    使用字符串 + 列表表达式
+    """
+    def isHappy(self, n: int) -> bool:
+        loopSet = set()
+        while n != 1:
+            if n in loopSet:
+                return False
+
+            loopSet.add(n)
+            n = sum([int(i)**2 for i in str(n)])
+
+        return True
+
+class Solution2:
+    """
+    求出一个数每个数位的平方和
+    """
+    def isHappy(self, n: int) -> bool:
+        def sumOfSquareDigit(n: int)->int:
+            SUM = 0
+            while n>9:
+                n, rem = divmod(n, 10)
+                SUM += rem**2
+            return SUM + n**2
+        loopSet = set()
+        while n!=1:
+            if n in loopSet:
+                return False
+
+            loopSet.add(n)
+            n = sumOfSquareDigit(n)
+
+        return True
