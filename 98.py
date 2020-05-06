29fe8de76fa05acbbf6ca601764e44ee8de8c4b8
zhangning <1305113016@qq.com>
2020-05-06 16:59:30 +0800

    验证BST有效性

--- 98.py
+++ 98.py
@@ -29,3 +29,30 @@ class Solution:
 
         return root.val>pval and self.onRight(root.left, pval) \
             and self.onRight(root.right, pval)
+
+class Solution2:
+    """
+    仅需验证 BST 的有序性即可，即验证当前节点与其左右子节点的关系
+    """
+    def isValidBST(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.onLeft(root.left, root.val) and\
+               self.onRight(root.right, root.val) and\
+               self.isValidBST(root.left) and \
+               self.isValidBST(root.right)
+
+        # 验证左子节点
+    def onLeft(self, left: TreeNode, pVal: int)-> bool:
+        if left is None:
+            return True
+        return left.val < pVal and\
+               self.onLeft(left.left, pVal) and\
+               self.onLeft(left.right, pVal)
+
+    def onRight(self, right: TreeNode, pVal: int) -> bool:
+        if right is None:
+            return True
+        return pVal < right.val and\
+               self.onRight(right.left, pVal) and\
+               self.onRight(right.right, pVal)
