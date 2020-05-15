943d1ae908f77f949108845d102fc2b804c685af
zhangning <1305113016@qq.com>
2020-05-15 11:56:13 +0800

    求回文子串的数目

--- 647.py
+++ 647.py
@@ -14,6 +14,25 @@ class Solution:
             return True
         return s==s[::-1]
 
+class Solution2:
+
+    def countSubstrings(self, s: str) -> int:
+        def count(s: str, begin: int, end: int):
+            res = 0
+            while begin>=0 and end<len(s) and s[begin]==s[end]:
+                res += 1
+                begin -= 1
+                end += 1
+            return res
+
+        res = 0
+        for i in range(len(s)):
+            res += count(s, i, i) # 长度为 奇数 的回文串
+            res += count(s, i, i+1)
+
+        print(res)
+        return res
 
 
     #TODO: 6230591 50700 364484
+Solution2().countSubstrings("aaa")
