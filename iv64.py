a92dfde97a4068eed952f1983a355cf81eeff20b
zhangning <1305113016@qq.com>
2020-06-02 10:48:41 +0800

    求1+2+...+n的和，不能使用for、while循环的if判断语句

--- iv64.py
+++ iv64.py
@@ -6,3 +6,10 @@ class Solution:
         if n==1:
             return 1
         return n+self.sumNums(n-1)
+
+class Solution2:
+    """
+    不能使用for、while循环，数学法
+    """
+    def sumNums(self, n: int) -> int:
+        return (1+n)*(n)//2
