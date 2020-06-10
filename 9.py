be063f6cb03cc5cf557e0b8ba26a6737c0c27736
zhangning <1305113016@qq.com>
2020-06-10 09:55:53 +0800

    回文数的判断

---
+++ 9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        left, right = 0, len(x)-1
+        while left<right:
+            if x[left]!=x[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
