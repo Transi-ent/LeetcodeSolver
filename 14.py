ba1b673eb670be84e590cecee661a41eaf7c964d
zhangning <1305113016@qq.com>
2020-06-15 12:18:04 +0800

    最长公共前缀

--- 14.py
+++ 14.py
@@ -11,5 +11,18 @@ class Solution:
                 break
         return strs[0][:p1]
 
+class Solution2:
+    def longestCommonPrefix(self, strs: list) -> str:
+        if not strs:
+            return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i, ch in enumerate(s1):
+            if ch != s2[i]:
+                return s2[:i]
+
+        return s1
+
+
 res = Solution().longestCommonPrefix(["dog","racecar","car"])
 print(res)
