36812984c587365aa749c6f7e0ad5fd494a0dec6
zhangning <1305113016@qq.com>
2020-07-03 11:39:01 +0800

    二叉树的层序遍历II

---
+++ iv32-3.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> list:
+        if root is None:
+            return []
+        res = []
+        q = [root]
+        level = 0
+        while q:
+            tmp = [] # list of level value
+            nodes = [] # list of next layer nodes
+            while q:
+                root = q.pop(0)
+                tmp.append(root.val)
+                if root.left: nodes.append(root.left)
+                if root.right: nodes.append(root.right)
+
+            q = nodes
+            tmp = tmp[::-1] if level%2==1 else tmp
+            res.append(tmp)
+            level += 1
+        return res
