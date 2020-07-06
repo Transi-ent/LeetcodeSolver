450987a8736230952dd9c1d147a563f7fcc4bb1d
zhangning <1305113016@qq.com>
2020-07-06 12:01:49 +0800

    到达目标网格的路径总数

--- 63.py
+++ 63.py
@@ -15,10 +15,30 @@ class Solution:
         # 因为数组 dp 多了一个元素
         return dp[-2]
 
+class Solution2:
+    """
+    动态规划：dp[i][j]表示从(1, 1)点到(i, j)有多少种不同的路径
+    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid: list) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        dp[1][1] = 1 ## 从起点到该点本身只有1条路
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i+1][j+1] = 0
+                else:
+                    if i == 0 and j == 0:
+                        continue
+                    dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j]
+        print(dp)
+        return dp[m][n]
 
-res = Solution().uniquePathsWithObstacles([
+
+res = Solution2().uniquePathsWithObstacles([
     [0,0,0],
-    [0,1,0],
+    [0,1,1],
     [0,0,0]
 ])
 print(res)
