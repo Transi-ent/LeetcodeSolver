03360a66755fd00349e992163c027658e93320f7
zhangning <1305113016@qq.com>
2020-07-07 10:25:36 +0800

    合并两个有序链表

---
+++ iv25.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    """
+    使用双指针+dummy Head
+    取得两个指针中较小的一个节点，放到dummy链的末尾
+    """
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(-1)
+        cur = dummy
+        while l1 and l2:
+            if l1.val<=l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
