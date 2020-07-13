da37f29316b485bd0034b405da316f6235722043
zhangning <1305113016@qq.com>
2020-07-13 10:59:05 +0800

    两个数组的交集

--- 350.py
+++ 350.py
@@ -14,3 +14,21 @@ def intersect(nums1: list, nums2: list) -> list:
             dict2[n] -= 1
 
     return res
+
+class Solution:
+    def intersect(self, nums1: list, nums2: list) -> list:
+        dic = {}
+        for em in nums1:
+            dic[em] = dic.get(em, 0) + 1
+
+        res = []
+        for em in nums2:
+            if em in dic:
+                res.append(em)
+                dic[em] -= 1
+                if dic[em]==0:
+                    del dic[em]
+
+        return res
+
+print(Solution().intersect([1,2,2,1], [2,2]))
