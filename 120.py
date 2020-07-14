f4829e2685786baa0ea813a279e3ff2433c06107
zhangning <1305113016@qq.com>
2020-07-14 11:01:44 +0800

    三角形最小路径和

--- 120.py
+++ 120.py
@@ -1,23 +1,6 @@
+import copy
 class Solution:
     def minimumTotal(self, triangle: list) -> int:
-        #TODO: Wrong, 这是贪心
-        pre = 0
-        res = 0
-        for i in range(len(triangle)):
-            if i == 0:
-                res += triangle[0][0]
-                pre = 0
-            else:
-                # i >= 1
-                if triangle[i][pre]>triangle[i][pre+1]:
-                    res += triangle[i][pre+1]
-                    pre += 1
-                else:
-                    res += triangle[i][pre]
-
-        return res
-
-    def minimumTotal2(self, triangle: list) -> int:
         if len(triangle)==0:
             return 0
         if len(triangle)==1:
@@ -37,20 +20,61 @@ class Solution:
 
         return min(dp[-1])
 
+class Solution2:
+    """
+    使用动态规划，dp[i][j] 表示到达位置 [i][j] 处时的最小路径和
+    dp[i][j] = triangle[i][j] + min(dp[i-1][j-1], dp[i-1][j])
+    Drawback: 该算法的空间复杂度为O(N^2)
+
+    """
+    def minimumTotal(self, triangle: list) -> int:
+        if len(triangle)<2:
+            return triangle[0][0]
+
+        dp = copy.deepcopy(triangle)
+        for i, lyst in enumerate(triangle):
+            for j, num in enumerate(lyst):
+                if i>0:
+                    if j==0:# 对于dp矩阵的第一列元素
+                        dp[i][j] = dp[i-1][j] + triangle[i][j]
+                    elif j==i:
+                        dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                    else:
+                        dp[i][j] = min( dp[i-1][j-1], dp[i-1][j] ) + triangle[i][j]
+        return min(dp[-1])
+
+
+class Solution3:
+    """
+    使用动态规划，dp[i][j] 表示到达位置 [i][j] 处时的最小路径和
+    dp[i][j] = triangle[i][j] + min(dp[i-1][j-1], dp[i-1][j])
+    修改方案：因为dp数组是按行计算，triangle数组每次只会用到本行的，不会用之前行数据，所以可以将计算得到的
+    dp数组覆盖在 triangle 上，此时空间复杂度为O(1)。
+    """
+
+    def minimumTotal(self, triangle: list) -> int:
+        if len(triangle) < 2:
+            return triangle[0][0]
+
+
+        for i, lyst in enumerate(triangle):
+            for j, num in enumerate(lyst):
+                if i > 0:
+                    if j == 0:  # 对于dp矩阵的第一列元素
+                        triangle[i][j] += triangle[i - 1][j]
+                    elif j == i:
+                        triangle[i][j] += triangle[i - 1][j - 1]
+                    else:
+                        triangle[i][j] += min(triangle[i - 1][j - 1], triangle[i - 1][j])
+        return min(triangle[-1])
+
+
+
+
+
 
 res = Solution().minimumTotal2([[2],[3,4],[6,5,7],[4,1,8,3]])
 print(res)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
