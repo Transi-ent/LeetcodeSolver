bec7ba17da686531221d3a431656d9aaaf65b13c
zhangning <1305113016@qq.com>
2020-07-15 11:03:41 +0800

    不同的二叉搜索树

--- 96.py
+++ 96.py
@@ -26,4 +26,32 @@ class Solution:
         print(memo)
         return memo[-1]
 
-Solution().numTrees(6)
+class Solution2:
+    """
+    使用记忆化搜索的方法，存储已经解决过的重复子问题。
+    因为要求n 个节点组成的BST结构有多少种，假设该问题为f(n) = dp[n]
+    （1）当n%2==1，(n-1)%2==0
+    f(n) = [f(n-1)+f(n-2)+...f(n-1/2 +1)]*2 + f(n-1/2)
+    (2) 当n%2==0，
+    f(n) = [f(n-1)+f(n-2)+...f(n/2)]*2
+    """
+    def numTrees(self, n: int) -> int:
+        dp = [0 for _ in range(n+1)]
+        dp[:2] = 1,1
+        if n<2:
+            return dp[n]
+
+        # 不断填充数组，直至n
+        for i in range(2, n+1):
+            for small in range(0, i):
+                big = i - 1 - small
+                if small<big:
+                    dp[i] += dp[big]*dp[small]*2
+                elif small==big:
+                    dp[i] += dp[big]*dp[small]
+                else:
+                    break
+
+        return dp[-1]
+
+print(Solution2().numTrees(6))
