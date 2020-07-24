7e1bf11ee5b452d4b5dc59a33cb5ce353f723e27
zhangning <1305113016@qq.com>
2020-07-24 13:19:36 +0800

    最长无重复子串

--- iv48.py
+++ iv48.py
@@ -17,4 +17,19 @@ class Solution:
 
         return res
 
-print(Solution().lengthOfLongestSubstring('abcabcbb'))
+class Solution2:
+    """
+    使用滑动窗口，滑动窗口内的字符都是不重复字符，时间复杂度为O(N)的实现;
+    使用hash表来记录当前字符出现的位置，因为字符的种类数最多128，所以空间复杂度为O(N);
+    这是一个右闭做开的区间；
+    """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        bag, res, left = {}, 0, -1
+        for right in range(len(s)):
+            if s[right] in bag:
+                left = max(left, bag[s[right]])
+            bag[s[right]] = right
+            res = max(res, right-left)
+        return res
+
+print(Solution2().lengthOfLongestSubstring('abcabcbb'))
