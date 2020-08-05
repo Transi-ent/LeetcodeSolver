af89d533f8788ea19bd12a4fdc9aa1d04a6bb1b0
zhangning <1305113016@qq.com>
2020-08-05 10:28:39 +0800

    顺时针打印矩阵

---
+++ iv29.py
@@ -0,0 +1,29 @@
+class Solution:
+    def spiralOrder(self, matrix: list) -> list:
+
+        res = []
+
+        while matrix:
+            # 1. 取上层
+            res += matrix.pop(0)
+
+            # 2. 取右
+            if matrix:
+                right = [l.pop() for l in matrix]
+                res += right
+            # 过滤空列表元素
+            matrix = [i for i in matrix if i]
+            # 3. 取下
+            if matrix:
+                down = matrix.pop()[::-1]
+                res += down
+
+            # 4. 取左
+            if matrix:
+                left = [l.pop(0) for l in matrix][::-1]
+                res += left
+            matrix = [i for i in matrix if i]
+        return res
+
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+print(Solution().spiralOrder(matrix))
