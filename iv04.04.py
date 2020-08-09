24b1552448cc4e8729d9b50b5a7a2a4c24befc39
zhangning <1305113016@qq.com>
2020-08-09 11:01:47 +0800

    判断二叉树的平衡性

---
+++ iv04.04.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(root: TreeNode)->int:
+            if root is None:
+                return 0
+            return max(height(root.left), height(root.right)) + 1
+        if root is None:
+            return True
+        if abs(height(root.left) - height(root.right))>1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
