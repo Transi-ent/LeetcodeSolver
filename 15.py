a70cd7a192c72e07134144c996c907cf14ca3b65
zhangning <1305113016@qq.com>
2020-09-13 23:26:25 +0800

    三数之和（需要去重）

--- 15.py
+++ 15.py
@@ -114,5 +114,50 @@ class Solution3:
 
         return res
 
-res = Solution3().threeSum([-1,0,1,2,-1,-4])
+class Solution11:
+    """
+    先排序，排完序后，使用一个for循环，每次固定最左侧元素，右边使用对撞指针；
+    """
+    def threeSum(self, nums: list) -> list:
+        n = len(nums)
+        if n<3:
+            return []
+        res = []
+        nums.sort()
+        for i in range(n-2):
+            if nums[i]>0:# 对于已排序列表，如果第一个元素>0, 三数中和比大于0
+                break
+
+            # 若当前元素与上一个元素相同，则跳过，避免重复
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            j, k = i+1, n-1
+            while j<k:
+                tmp = nums[i]+nums[j]+nums[k]
+                if tmp<0:
+                    j += 1
+                    while j<k and nums[j]==nums[j-1]:
+                        j += 1
+
+                elif tmp>0:
+                    k -= 1
+                    while j<k and nums[k]==nums[k+1]:
+                        k -= 1
+
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j<k and nums[j]==nums[j-1]: j += 1
+                    while j<k and nums[k]==nums[k+1]: k -= 1
+
+        return res
+
+
+
+
+
+
+res = Solution11().threeSum([-1,0,1,2,-1,-4])
+
 print(res)
